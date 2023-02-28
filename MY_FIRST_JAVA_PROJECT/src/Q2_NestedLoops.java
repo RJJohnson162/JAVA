e062143af74fca18b7f0f24bbafb0a3ff1a10942
RJJohnson162 <91172411+RJJohnson162@users.noreply.github.com>
2023-02-28 21:34:24 +0300

    hot fix

--- MY_FIRST_JAVA_PROJECT/src/Q2_NestedLoops.java
+++ MY_FIRST_JAVA_PROJECT/src/Q2_NestedLoops.java
@@ -6,7 +6,7 @@ public class Q2_NestedLoops {
 
         for (int i = 1; i <= numRows; i++) {
             for (int j = 1; j <= numRows; j++) {
-                if (i == j) {
+                if ((i == j) || ((i == 1) || (i == 6)) ) {
                     System.out.print("& ");
                 } else {
                     System.out.print("* ");
