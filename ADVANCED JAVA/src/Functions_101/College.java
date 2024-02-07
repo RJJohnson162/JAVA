11a8fd299a0141cfc8faac468a0db87413d881b8
RJJohnson162 <91172411+RJJohnson162@users.noreply.github.com>
2024-02-07 16:47:25 +0300

    college

--- ADVANCED JAVA/src/Functions_101/College.java
+++ ADVANCED JAVA/src/Functions_101/College.java
@@ -1,64 +1,67 @@
 package Functions_101;
 import java.util.Scanner;
+
 public class College {
-    private static double Programming;
-    private static double Web_Design;
-    private static double Networking;
-    private double Total;
-    private double Average;
+    private double programming;
+    private double webDesign;
+    private double networking;
+    private double total;
+    private double average;
 
-    public College(double Programming, double Web_Design, double Networking){
-        College.Programming = Programming;
-        College.Web_Design = Web_Design;
-        College.Networking = Networking;
-    }
-    public College(){
-        College.Networking= 0.0;
-        College.Programming = 0.0;
-        College.Web_Design = 0.0;
+    public College(double programming, double webDesign, double networking) {
+        this.programming = programming;
+        this.webDesign = webDesign;
+        this.networking = networking;
     }
 
-    public double getAverage(){
-        return Average;
-    }
-    public double getTotal(){
-        return Total;
+    public College() {
+        this(0.0, 0.0, 0.0);
     }
 
-    public static double data_in(String message, Scanner Input){
-        try{
-            System.out.print(message);
-            return Input.nextDouble();
-        } catch (Exception e){
-            Input.next();
-            System.out.println("Invalid input!!");
-            return data_in(message, Input);
+    public double getAverage() {
+        return average;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void compute() {
+        total = programming + networking + webDesign;
+        average = total / 3;
+    }
+
+    public void dataPrompt() {
+        try (Scanner input = new Scanner(System.in)) {
+            String[] units = {"Programming", "Networking", "Web Design"};
+            programming = dataIn("Enter the marks for " + units[0] + ".\n", input);
+            networking = dataIn("Enter the marks for " + units[1] + ".\n", input);
+            webDesign = dataIn("Enter the marks for " + units[2] + ".\n", input);
         }
     }
-    public void compute(){
-        Total = Programming + Networking + Web_Design;
-        Average = Total/3;
 
-    }
-    public void dataPrompt(){
-        Scanner Input = new Scanner(System.in);
-        String[] Units = {"Programming", "Networking", "Web Design"};
-        Programming = data_in("Enter the marks for"+Units[0]+".\n",Input);
-        Networking = data_in("Enter the marks for"+Units[1]+".\n",Input);
-        Web_Design = data_in("Enter the marks for"+Units[2]+".\n",Input);
-    }
-    public void dataOut(){
+    public void dataOut() {
         System.out.println("\n\t__________\t RESULTS \t__________\n");
-        System.out.println("The marks for Programming are: \t\t\t"+Programming+".");
-        System.out.println("The marks for Networking are: \t\t\t"+Networking+".");
-        System.out.println("The marks for Web_Design are: \t\t\t"+Web_Design+".");
-        System.out.println("The Total mark is: \t\t\t\t"+Total+".");
-        System.out.println("The average for the marks is: \t\t\t"+Average+".");
-
+        System.out.println("The marks for Programming are: \t\t\t" + programming + ".");
+        System.out.println("The marks for Networking are: \t\t\t" + networking + ".");
+        System.out.println("The marks for Web Design are: \t\t\t" + webDesign + ".");
+        System.out.println("The Total mark is: \t\t\t\t" + total + ".");
+        System.out.println("The average for the marks is: \t\t\t" + average + ".");
     }
-}
-class Main {
-    public static void main(String[] args){
+
+    private static double dataIn(String message, Scanner input) {
+        while (true) {
+            try {
+                System.out.print(message);
+                return input.nextDouble();
+            } catch (Exception e) {
+                input.next();
+                System.out.println("Invalid input!!");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
         College college1 = new College();
         college1.dataPrompt();
         college1.compute();
